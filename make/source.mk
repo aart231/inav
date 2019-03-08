8c1aa78b7354827ae2738f877ecce8437312efc8
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2019-03-08 14:21:14 +0100

    First cut on data structure

--- make/source.mk
+++ make/source.mk
@@ -11,6 +11,7 @@ COMMON_SRC = \
             common/encoding.c \
             common/filter.c \
             common/logic_condition.h \
+            common/global_functions.h \
             common/maths.c \
             common/calibration.c \
             common/memory.c \
